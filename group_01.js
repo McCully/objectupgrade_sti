c98214af95619ce058fec6035ce5fbdf26866e75
Casie Lynch <cds.lynch@gmail.com>
2016-07-28 14:21:55 -0500

    initial commit

--- group_01.js
+++ group_01.js
@@ -13,6 +13,11 @@ function calculateSTI(empInfo){
 
     var bonusPercent = 0;
 
+    var processedEmployee = [];
+
+    var bonusMoney = 0;
+
+    var totalSalary = 0;
 
     switch(rating){
         case 0:
@@ -32,4 +37,41 @@ function calculateSTI(empInfo){
         default:
             bonusPercent = 0;
     }
+
+    bonusPercent = adjustBonusPercent(empNumber, bonusPercent, currentSalary);
+    console.log(bonusPercent);
+
+    processedEmployee[0] = name;
+    processedEmployee[1] = bonusPercent;
+
+    bonusMoney = Math.round(bonusPercent * currentSalary);
+    totalSalary = bonusMoney + currentSalary;
+
+    processedEmployee[2] = totalSalary;
+    processedEmployee[3] = bonusMoney;
+
+    return processedEmployee;
+
 }
+
+function adjustBonusPercent(empNumber, bonusPercent, currentSalary){
+    console.log("first parameter check ", bonusPercent)
+    if(empNumber.length == 4){
+        bonusPercent += .05;
+        console.log(bonusPercent);
+    }
+
+    if(currentSalary > 65000){
+        bonusPercent -= .01;
+    }
+
+    if(bonusPercent > .13){
+        bonusPercent = .13;
+    }
+    return bonusPercent;
+}
+
+for(var i = 0; i < employees.length ; i++){
+    console.log(calculateSTI(employees[i]));
+}
+
