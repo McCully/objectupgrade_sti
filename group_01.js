1b31f81a88c98c25a7de3c502bc73f2077b4604a
Casie Lynch <cds.lynch@gmail.com>
2016-07-28 13:52:36 -0500

    initial commit

---
+++ group_01.js
@@ -0,0 +1,35 @@
+var atticus = ["Atticus", "2405", "47000", 3];
+var jem = ["Jem", "62347", "63500", 4];
+var boo = ["Boo", "11435", "54000", 3];
+var scout = ["Scout", "6243", "74750", 5];
+
+var employees = [atticus, jem, boo, scout];
+
+function calculateSTI(empInfo){
+    var name = empInfo[0];
+    var empNumber = empInfo[1];
+    var currentSalary = Math.round(parseFloat(empInfo[2]));
+    var rating = empInfo[3];
+
+    var bonusPercent = 0;
+
+
+    switch(rating){
+        case 0:
+        case 1:
+        case 2:
+            bonusPercent = 0;
+            break;
+        case 3:
+            bonusPercent = .04;
+            break;
+        case 4:
+            bonusPercent = .06;
+            break;
+        case 5:
+            bonusPercent = .10;
+            break;
+        default:
+            bonusPercent = 0;
+    }
+}
